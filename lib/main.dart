98ba9da5fd30025c6439c286a9476aedbd712231
flutterd3 <107663176+flutterd3@users.noreply.github.com>
2022-07-08 07:00:44 +0545

    firebase integration competed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/get_instance.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:tiktokclone/constants.dart';
 import 'package:tiktokclone/views/screens/auth/login_screen.dart';
 import 'package:tiktokclone/views/screens/auth/signup_screen.dart';
 
-void main() {
+import 'controllers/auth_controller.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value) {
+    Get.put(AuthController());
+  });
   runApp(const MyApp());
 }
 
@@ -12,7 +22,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Tiktok Clone",
       theme: ThemeData.dark().copyWith(
